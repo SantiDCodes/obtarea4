8e12ce12290725fead9ea9ad6189886eab061958
Santiago G Delgado C <santidc.programmer@gmail.com>
2022-07-24 19:43:58 -0300

    iNICIO TAREA NUMERO 4 OPEN BOOTCAMP

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Tarea 4 Open BootCamp");
+        System.out.println("HHerencia, Polimorfismo, Intefaces.");
+        System.out.println("");
+        System.out.println("*****************************************");
+        System.out.println("");
+        System.out.println(">>INICIO<<");
+    }
+}
